5d522e19032f3a209f33bc52387873b0a90efd07
Elylou <audrey.stammler@outlook.fr>
2018-04-23 11:00:22 +0200

    revision des proprotion home

--- content/themes/find-your-job/app/initialize.js
+++ content/themes/find-your-job/app/initialize.js
@@ -16,7 +16,23 @@ $(".right").click(function(){
     $(".carousel").carousel("next");
 });
 
+$(document).mousemove(function(event) {
+  windowWidth = $(window).width();
+  windowHeight = $(window).height();
 
+  mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
+  mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
+
+  $('.radial-gradient').css('background', 'radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, #3498db, #9b59b6)');
+});$(document).mousemove(function(event) {
+  windowWidth = $(window).width();
+  windowHeight = $(window).height();
+
+  mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
+  mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
+
+  $('.radial-gradient').css('background', 'radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, #3498db, #9b59b6)');
+});
 //Ajout des action js dans la fontion init!
 $(app.init);
 
