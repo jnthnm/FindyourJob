cef8700bd9a45bec788eb958433869c0fa41aa7e
Jonathan M <nknlol75@gmail.com>
2018-04-25 15:30:45 +0200

    page profil et paiement terminer

---
+++ content/themes/find-your-job/login.php
@@ -0,0 +1,28 @@
+<?php
+// Formulaire de connexion
+if ( ! is_user_logged_in() ) {
+	wp_login_form( array(
+        'redirect'       => site_url( '/' ), // par défaut renvoie vers la page courante
+        'label_username' => 'Login',
+        'label_password' => 'Mot de passe',
+        'label_remember' => 'Se souvenir de moi',
+        'label_log_in'   => 'Se connecter',
+        'form_id'        => 'login-form',
+        'id_username'    => 'user-login',
+        'id_password'    => 'user-pass',
+        'id_remember'    => 'rememberme',
+        'id_submit'      => 'wp-submit',
+        'remember'       => true, //afficher l'option se ouvenir de moi
+        'value_remember' => false //se souvenir par défaut ?
+	) );
+} else {
+	echo '<a href="' . admin_url( 'user-edit.php?user_id='. get_current_user_id() ) .'">Accès au profil</a>';
+	echo '<a href="' . wp_logout_url( site_url( '/' ) ) .'">Se déconnecter</a>';
+}
+
+// Ajouter le lien pour récupérer le mot de passe, si l'utilisateur ne s'en souvient plus
+add_filter( 'login_form_bottom', 'lien_mot_de_passe_perdu' );
+function lien_mot_de_passe_perdu( $formbottom ) {
+	$formbottom .= '<a href="' . wp_lostpassword_url() . '">Mot de passe perdu ?</a>';
+	return $formbottom;
+}
