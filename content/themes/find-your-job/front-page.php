5bcf5b81c8eea6152c53250aef9a3bf41af401e6
Jonathan M <nknlol75@gmail.com>
2018-04-27 01:36:26 +0200

    Ajout du lien sur INFOS de la home, ajout du fichier connectionTEST pour l'espace membre (brandao pls).

--- content/themes/find-your-job/front-page.php
+++ content/themes/find-your-job/front-page.php
@@ -1,3 +1,10 @@
+
+<?php
+/*
+Template Name: Home
+*/
+?>
+
 <?php get_header();?>
 
 
--- content/themes/find-your-job/template-parts/section/concour.php
+++ content/themes/find-your-job/template-parts/section/concour.php
@@ -5,12 +5,13 @@
              <h3 class="listen_concour_title">Participer a notre jeu concours</h3>
              <p class="listen_concour-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
              <br>
-             <button type="button" class="btn btn-outline-primary btn-rounded btn-click" href="<?php echo get_permalink( get_page_by_title( 'concour' ) )?>">Cliquez</button>
+             <a class="btn but_white" href="<?php echo get_permalink( get_page_by_title( 'concour' ) )?>" role="button">infos</a>
          </div> <!--listen-content-->
+
            </div><!--col-->
 
 
-        <div class=" listen_equip">
+
           <h3 class="listen_equip_title">Une équipe a votre écoute</h3> <br>
           <p class="listen_equip_content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
           <br><br>
