05f1de4e3f87c78d931c52c8236d40ca3408d927
Spiral13 <cyno13@msn.com>
2018-04-27 19:05:10 +0200

    Résolution de bug CSS

--- content/themes/find-your-job/template-parts/section/concour.php
+++ content/themes/find-your-job/template-parts/section/concour.php
@@ -7,14 +7,14 @@
              <br>
 
 
-             <a class="btn but_white ui-button" href="<?php echo get_permalink( get_page_by_title( 'concour' ) )?>" role="button">Cliquez</a>
+             <a class="btn buty_white ui-button " href="<?php echo get_permalink( get_page_by_title( 'concour' ) )?>" role="button">Cliquez</a>
 
          </div> <!--listen-content-->
 
            </div><!--col-->
 
 
-
+    <div class=" listen_equip">
           <h3 class="listen_equip_title">Une équipe a votre écoute</h3> <br>
           <p class="listen_equip_content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
           <br><br>
