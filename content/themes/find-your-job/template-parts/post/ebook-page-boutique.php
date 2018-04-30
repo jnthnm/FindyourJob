46ac99b2ad692ed40cb5c3134c301620b640f169
Elylou <audrey.stammler@outlook.fr>
2018-04-30 10:26:14 +0200

    ajout woocommerce

---
+++ content/themes/find-your-job/template-parts/post/ebook-page-boutique.php
@@ -0,0 +1,59 @@
+
+                  <div class="section_ebook_article">
+                      <img class="section_ebook_img" src="<? the_post_thumbnail(); ?>" alt="Livres">
+                      <h3 class="section_ebook_article_title"><? the_title(); ?></h3>
+                      <p class="section_ebook_article_desc"><? the_excerpt(); ?></p>
+                      <div class="section_ebook_price"> <? the_fields('price_ebook'); ?></div>
+                      <a class="btn btn-primary button_price" href="#" role="button">Ajouter au panier</a>
+                  </div>
+
+<!--
+
+                  <div class="section_ebook_article">
+                      <img class="section_ebook_img" src="images/ebook/ebook2.png" alt="Livres">
+                      <h3 class="section_ebook_article_title">Lorem ipsum</h3>
+                      <p class="section_ebook_article_desc">Lorem ipsum dolor sit amet, consectetur</p>
+                      <div class="section_ebook_price">5.99€</div>
+                      <a class="btn btn-primary button_price" href="#" role="button">Ajouter au panier</a>
+                  </div>
+
+
+
+                  <div class="section_ebook_article">
+                      <img class="section_ebook_img" src="images/ebook/ebook3.png" alt="Livres">
+                      <h3 class="section_ebook_article_title">Lorem ipsum</h3>
+                      <p class="section_ebook_article_desc">Lorem ipsum dolor sit amet, consectetur</p>
+                      <div class="section_ebook_price">5.99€</div>
+                      <a class="btn btn-primary button_price" href="#" role="button">Ajouter au panier</a>
+                  </div>
+
+
+
+                  <div class="section_ebook_article">
+                      <img class="section_ebook_img" src="images/ebook/ebook4.png" alt="Livres">
+                      <h3 class="section_ebook_article_title">Lorem ipsum</h3>
+                      <p class="section_ebook_article_desc">Lorem ipsum dolor sit amet, consectetur</p>
+                      <div class="section_ebook_price">5.99€</div>
+                      <a class="btn btn-primary button_price" href="#" role="button">Ajouter au panier</a>
+                  </div>
+
+
+
+                  <div class="section_ebook_article">
+                      <img class="section_ebook_img" src="images/ebook/ebook5.png" alt="Livres">
+                      <h3 class="section_ebook_article_title">Lorem ipsum</h3>
+                      <p class="section_ebook_article_desc">Lorem ipsum dolor sit amet, consectetur</p>
+                      <div class="section_ebook_price">5.99€</div>
+                      <a class="btn btn-primary button_price" href="#" role="button">Ajouter au panier</a>
+                  </div>
+
+
+
+                  <div class="section_ebook_article">
+                      <img class="section_ebook_img" src="images/ebook/ebook1.png" alt="Livres">
+                      <h3 class="section_ebook_article_title">Lorem ipsum</h3>
+                      <p class="section_ebook_article_desc">Lorem ipsum dolor sit amet, consectetur</p>
+                      <div class="section_ebook_price">5.99€</div>
+                      <a class="btn btn-primary button_price" href="#" role="button">Ajouter au panier</a>
+                  </div>
+                  </div> -->
