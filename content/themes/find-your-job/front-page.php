4f81516671cf1ccf0a371da6a0b27002cd6cdd7b
Jonathan M <nknlol75@gmail.com>
2018-05-07 10:02:19 +0200

    correction front-page

--- content/themes/find-your-job/front-page.php
+++ content/themes/find-your-job/front-page.php
@@ -11,22 +11,6 @@ Template Name: Home
 <!--==========main==========-->
  <main>
 
-
-   <!--==========API REST==========-->
-   <?php
-   define('BASEURL','http://localhost/Find-your-jOb/');
-   define('JSONURL', 'wp-json/wp/v2/');
-
-   $json = file_get_contents(BASEURL . JSONURL . 'posts');
-
-   $json_decode = json_decode($json); // <<= function json_decode -> permet de décoder le json pour le transformer en tableau php.
-
-  // header ('Content-type: application/json'); <<= je fais comprendre au nav que je souhaite afficher du json.
-  echo '<pre>';
-  var_dump($json_decode);
-
-   ?>
-
 <?php   get_template_part('template-parts/header/carousel'); // carousel
 
 //<!--==========media==========-->
